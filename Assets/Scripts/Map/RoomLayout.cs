c9cf4efd4669992d92fcc66739efe8a9fea173d5
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-04-10 01:10:53 -0400

    about to implement dict chain

--- Assets/Scripts/Map/RoomLayout.cs
+++ Assets/Scripts/Map/RoomLayout.cs
@@ -17,10 +17,10 @@ public class RoomLayout : MonoBehaviour {
         
     }
 
-    public HashSet<Vector3> FindWallVerticies() {
+    public Dictionary<Vector2,Vector2> FindWallVerticies() {
         Vector2[] directs = new Vector2[]{Vector2.right,Vector2.down,Vector2.left,Vector2.up};
 
-        HashSet<Vector3> wall_verticies = new HashSet<Vector3>();
+        Dictionary<Vector2,Vector2> wall_verticies = new Dictionary<Vector2,Vector2>();
         for (int y = 0; y < this.grid.GetLength(1); y++) {
             for (int x = 0; x < this.grid.GetLength(0); x++) {
 
@@ -37,21 +37,17 @@ public class RoomLayout : MonoBehaviour {
 
                         string newval = this.grid[nx, ny];
                         //TODO: make this nicer to the eyes
-                        //add wall verticies
+                        //add wall verticies, the order of the verticies forces a clockwise traversal
                         if (newval == "#") {
                             
-                            if (d == Vector2.right) {
-                                wall_verticies.Add(new Vector3(x+1,y));
-                                wall_verticies.Add(new Vector3(x+1,y+1));
-                            } else if (d == Vector2.down) {
-                                wall_verticies.Add(new Vector3(x+1,y+1));
-                                wall_verticies.Add(new Vector3(x,y+1));
-                            } else if (d == Vector2.left) {
-                                wall_verticies.Add(new Vector3(x,y+1));
-                                wall_verticies.Add(new Vector3(x,y));
-                            } else if (d == Vector2.up) {
-                                wall_verticies.Add(new Vector3(x,y));
-                                wall_verticies.Add(new Vector3(x+1,y));
+                            if (d == Vector2.right) { //left wall
+                                new WallSegment(new Vector2(x,y+1),new Vector2(x,y));
+                            } else if (d == Vector2.down) { //upper wall
+                                new WallSegment(new Vector2(x,y),new Vector2(x+1,y));
+                            } else if (d == Vector2.left) { //right wall
+                                new WallSegment(new Vector2(x+1,y),new Vector2(x+1,y+1));
+                            } else if (d == Vector2.up) { //bottom wall
+                                new WallSegment(new Vector2(x+1,y+1),new Vector2(x,y+1));
                             }
                         }
                     }
@@ -67,3 +63,15 @@ public class RoomLayout : MonoBehaviour {
     }
 
 }
+
+public class WallSegment {
+    public Vector2 start;
+    public Vector2 end;
+
+    public WallSegment(Vector2 start, Vector2 end) {
+        this.start = start;
+        this.end = end;
+    }
+
+}
+
