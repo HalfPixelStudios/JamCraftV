9593d474a9c9572cbb4b0ff9b7ecd5ef7fe41581
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-04-11 20:56:34 -0400

    adding globalcontainer

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,65 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Rigidbody pRb;
+    private Vector3 offset;
+    private float shakeAmt;
 
-    [SerializeField] [Range(0f, 10f)] private float cameraSpeed;
-    public GameObject target;
-    public float shakeAmt;
-    public float shakeTime;
-    public float shakeInterval;
-    private float _intervalDelta=0;
-    [SerializeField] private float shakeDelta = 0;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        pRb = player.GetComponent<Rigidbody>();
+        offset = transform.position - player.transform.position;
+        
+
+    }
+
+    //copied directly from internet
     
 
-    void Start() {
-
-    }
-
-    void Update()
+    
+    private void Update()
     {
-        transform.LookAt(target.transform);
-        if (GetComponent<Camera>().orthographicSize < 2||target==null)
-        {
-            return;
-        }
-        
-        float t = cameraSpeed * Time.deltaTime;
-        transform.position = new Vector3(Mathf.Lerp(transform.position.x, target.transform.position.x,t),transform.position.y,Mathf.Lerp(transform.position.z, target.transform.position.z,t));
+        transform.position = player.transform.position + offset;
 
-        //Camera pans towards mouse
-        //Vector2 mouse_pos = Input.mousePosition;
-        //transform.position = new Vector3(Mathf.Lerp(transform.position.x, mouse_pos.x, t), Mathf.Lerp(transform.position.y, mouse_pos.y, t), transform.position.z);
 
-        if (shakeDelta > 0)
-        {
-            if (_intervalDelta >=shakeInterval)
-            {
-                shake();
-                _intervalDelta= 0;
-            }
 
-            _intervalDelta += Time.deltaTime;
-            shakeDelta -= Time.deltaTime;
 
-        }
-        else
-        {
-            _intervalDelta = 0;
-        }
 
 
     }
-
-    public void startShake(float shake,float shakeTime)
-    {
-        shakeAmt = shake;
-        shakeDelta = shakeTime;
-
-    }
-
+    
     void shake()
     {
         if(shakeAmt>0) 
@@ -72,3 +43,4 @@ public class CameraController : MonoBehaviour {
         }
     }
 }
+
---
+++ Assets/Scripts/GlobalContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalContainer : MonoBehaviour
+{
+    public GameObject player;
+    public static GlobalContainer Global;
+    public GameObject inventory;
+
+    private void Awake()
+    {
+        Global = this;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
